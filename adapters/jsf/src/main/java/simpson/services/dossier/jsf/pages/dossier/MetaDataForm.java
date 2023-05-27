8ea41d74ef385b92bffbdc58668a459d7b5205d3
Benjamin Simpson <ben@bensimpson.ch>
2023-05-27 11:43:30 +0200

    DOS-6 implement jsf front end

---
+++ adapters/jsf/src/main/java/simpson/services/dossier/jsf/pages/dossier/MetaDataForm.java
@@ -0,0 +1,57 @@
+package simpson.services.dossier.jsf.pages.dossier;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.ejb.EJB;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+import simpson.services.dossier.document.*;
+import simpson.services.dossier.services.DocumentService;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Named
+@ViewScoped
+public class MetaDataForm implements Serializable {
+
+
+    private static final long serialVersionUID = -4673173986383109013L;
+
+    @EJB
+    private DocumentService documentService;
+
+    private Document document;
+    private String name;
+    private String description;
+
+    @PostConstruct
+    public void init() {
+        var metaData = (MetaData) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("selectedDocument");
+        this.document = this.documentService.readDocument(metaData.documentId());
+        this.name = this.document.metaData().name().value();
+        this.description = this.document.metaData().description().value();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void formActionListener() {
+        var updatedMetadata = new MetaData(document.id(), new Name(name), new Description(description), document.metaData().size(), new Modified(LocalDateTime.now()));
+        var updatedDocument = new Document(document.id(), document.content(), updatedMetadata);
+        documentService.saveDocument(updatedDocument);
+    }
+}
